96a54352ec6db944d9bbf002009b7b4ba980f92c
Matt-salis <m.salischiker@hotmail.com>
2021-08-30 15:21:21 -0300

    Fila 1 de requerimientos

--- src/main/java/com/example/airlinetickets/AirlineTicketsApplication.java
+++ src/main/java/com/example/airlinetickets/AirlineTicketsApplication.java
@@ -1,77 +1,90 @@
 package com.example.airlinetickets;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 
 @SpringBootApplication
 public class AirlineTicketsApplication {
 
-	public static void main(String[] args) {
+
+    public static void main(String[] args) {
 		SpringApplication.run(AirlineTicketsApplication.class, args);
-	}
 
-	@Bean
-	public CommandLineRunner initData(VueloRepository vueloRepository) {
-		return (args) -> {
-			Vuelo vuelo1 = new Vuelo("B542", "Mendoza", 5, LocalDate.of(2021,9,02));
-			Vuelo vuelo2 = new Vuelo("B545", "Mendoza", 4, LocalDate.of(2021,9,03));
-			Vuelo vuelo3 = new Vuelo("B534", "Mendoza", 2, LocalDate.of(2021,9,04));
-			Vuelo vuelo4 = new Vuelo("B577", "Mendoza", 2, LocalDate.of(2021,9,03));
-			Vuelo vuelo5 = new Vuelo("B571", "Mendoza", 3, LocalDate.of(2021,9,02));
-			Vuelo vuelo6 = new Vuelo("B511", "Mendoza", 1, LocalDate.of(2021,9,04));
-			Vuelo vuelo7 = new Vuelo("B520", "Mendoza", 7, LocalDate.of(2021,9,02));
-			Vuelo vuelo8 = new Vuelo("C633", "Tucuman", 5, LocalDate.of(2021,9,02));
-			Vuelo vuelo9 = new Vuelo("C648", "Tucuman", 3, LocalDate.of(2021,9,02));
-			Vuelo vuelo10 = new Vuelo("C639", "Tucuman", 8, LocalDate.of(2021,9,03));
-			Vuelo vuelo11 = new Vuelo("C682", "Tucuman", 4, LocalDate.of(2021,9,03));
-			Vuelo vuelo12 = new Vuelo("C677", "Tucuman", 5, LocalDate.of(2021,9,03));
-			Vuelo vuelo13 = new Vuelo("C691", "Tucuman", 2, LocalDate.of(2021,9,04));
-			Vuelo vuelo14 = new Vuelo("C616", "Tucuman", 6, LocalDate.of(2021,9,04));
-			Vuelo vuelo15 = new Vuelo("C644", "Tucuman", 7, LocalDate.of(2021,9,04));
-			Vuelo vuelo16 = new Vuelo("D855", "San Luis", 4, LocalDate.of(2021,9,02));
-			Vuelo vuelo17 = new Vuelo("D812", "San Luis", 6, LocalDate.of(2021,9,02));
-			Vuelo vuelo18 = new Vuelo("D842", "San Luis", 2, LocalDate.of(2021,9,02));
-			Vuelo vuelo19 = new Vuelo("D864", "San Luis", 4, LocalDate.of(2021,9,03));
-			Vuelo vuelo20 = new Vuelo("D866", "San Luis", 6, LocalDate.of(2021,9,03));
-			Vuelo vuelo21 = new Vuelo("D884", "San Luis", 2, LocalDate.of(2021,9,03));
-			Vuelo vuelo22 = new Vuelo("D854", "San Luis", 3, LocalDate.of(2021,9,04));
-			Vuelo vuelo23 = new Vuelo("D832", "San Luis", 5, LocalDate.of(2021,9,04));
-			Vuelo vuelo24 = new Vuelo("D874", "San Luis", 6, LocalDate.of(2021,9,04));
+		// Establezco una lista de objetos Vuelo como datos de prueba
 
-			vueloRepository.save(vuelo1);
-			vueloRepository.save(vuelo2);
-			vueloRepository.save(vuelo3);
-			vueloRepository.save(vuelo4);
-			vueloRepository.save(vuelo5);
-			vueloRepository.save(vuelo6);
-			vueloRepository.save(vuelo7);
-			vueloRepository.save(vuelo8);
-			vueloRepository.save(vuelo9);
-			vueloRepository.save(vuelo10);
-			vueloRepository.save(vuelo11);
-			vueloRepository.save(vuelo12);
-			vueloRepository.save(vuelo13);
-			vueloRepository.save(vuelo14);
-			vueloRepository.save(vuelo15);
-			vueloRepository.save(vuelo16);
-			vueloRepository.save(vuelo17);
-			vueloRepository.save(vuelo18);
-			vueloRepository.save(vuelo19);
-			vueloRepository.save(vuelo20);
-			vueloRepository.save(vuelo21);
-			vueloRepository.save(vuelo22);
-			vueloRepository.save(vuelo23);
-			vueloRepository.save(vuelo24);
+        List<Vuelo> vuelos = new ArrayList<>();
+        Vuelo vuelo1 = new Vuelo("B542", "Mendoza", 5, LocalDate.of(2021,9,02));
+        Vuelo vuelo2 = new Vuelo("B545", "Mendoza", 4, LocalDate.of(2021,9,03));
+        Vuelo vuelo3 = new Vuelo("B534", "Mendoza", 2, LocalDate.of(2021,9,04));
+        Vuelo vuelo4 = new Vuelo("B577", "Mendoza", 2, LocalDate.of(2021,9,03));
+        Vuelo vuelo5 = new Vuelo("B571", "Mendoza", 3, LocalDate.of(2021,9,02));
+        Vuelo vuelo6 = new Vuelo("B511", "Mendoza", 1, LocalDate.of(2021,9,04));
+        Vuelo vuelo7 = new Vuelo("B520", "Mendoza", 7, LocalDate.of(2021,9,02));
+        Vuelo vuelo8 = new Vuelo("C633", "Tucuman", 5, LocalDate.of(2021,9,02));
+        Vuelo vuelo9 = new Vuelo("C648", "Tucuman", 3, LocalDate.of(2021,9,02));
+        Vuelo vuelo10 = new Vuelo("C639", "Tucuman", 8, LocalDate.of(2021,9,03));
+        Vuelo vuelo11 = new Vuelo("C682", "Tucuman", 4, LocalDate.of(2021,9,03));
+        Vuelo vuelo12 = new Vuelo("C677", "Tucuman", 5, LocalDate.of(2021,9,03));
+        Vuelo vuelo13 = new Vuelo("C691", "Tucuman", 2, LocalDate.of(2021,9,04));
+        Vuelo vuelo14 = new Vuelo("C616", "Tucuman", 6, LocalDate.of(2021,9,04));
+        Vuelo vuelo15 = new Vuelo("C644", "Tucuman", 7, LocalDate.of(2021,9,04));
+        Vuelo vuelo16 = new Vuelo("D855", "San Luis", 4, LocalDate.of(2021,9,02));
+        Vuelo vuelo17 = new Vuelo("D812", "San Luis", 6, LocalDate.of(2021,9,02));
+        Vuelo vuelo18 = new Vuelo("D842", "San Luis", 2, LocalDate.of(2021,9,02));
+        Vuelo vuelo19 = new Vuelo("D864", "San Luis", 4, LocalDate.of(2021,9,03));
+        Vuelo vuelo20 = new Vuelo("D866", "San Luis", 6, LocalDate.of(2021,9,03));
+        Vuelo vuelo21 = new Vuelo("D884", "San Luis", 2, LocalDate.of(2021,9,03));
+        Vuelo vuelo22 = new Vuelo("D854", "San Luis", 3, LocalDate.of(2021,9,04));
+        Vuelo vuelo23 = new Vuelo("D832", "San Luis", 5, LocalDate.of(2021,9,04));
+        Vuelo vuelo24 = new Vuelo("D874", "San Luis", 6, LocalDate.of(2021,9,04));
+        vuelos.add(vuelo1);
+        vuelos.add(vuelo2);
+        vuelos.add(vuelo3);
+        vuelos.add(vuelo4);
+        vuelos.add(vuelo5);
+        vuelos.add(vuelo6);
+        vuelos.add(vuelo7);
+        vuelos.add(vuelo8);
+        vuelos.add(vuelo9);
+        vuelos.add(vuelo10);
+        vuelos.add(vuelo11);
+        vuelos.add(vuelo12);
+        vuelos.add(vuelo13);
+        vuelos.add(vuelo14);
+        vuelos.add(vuelo15);
+        vuelos.add(vuelo16);
+        vuelos.add(vuelo17);
+        vuelos.add(vuelo18);
+        vuelos.add(vuelo19);
+        vuelos.add(vuelo20);
+        vuelos.add(vuelo21);
+        vuelos.add(vuelo22);
+        vuelos.add(vuelo23);
+        vuelos.add(vuelo24);
 
 
+//        Enseño en consola los parametros utilizados que se aplicaran en este filtro especifico
+
+        System.out.println("Vuelos disponibles con los siguientes parametros:");
+        System.out.println("Destino: Mendoza");
 
 
-		};
-	}
+//        Realizo el filtro para que cumpla con el parametro establecido anteriormente y enseño en consola las coincidencias
+
+        List<Vuelo> vueloElegido = vuelos.stream().filter(x -> x.getDestino().equals("Mendoza")).collect(Collectors.toList());
+        vueloElegido.stream().forEach(x -> System.out.println(x.getNumeroDeVuelo() +" "+ x.getDestino() +" "+ x.getFecha()));
+
+
+    }
 
 }
+
